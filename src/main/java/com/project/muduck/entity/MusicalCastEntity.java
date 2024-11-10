c7908e48cca3af0101f1f5f67d8c427b78e075ad
workmakerdaily <sver12@naver.com>
2024-11-10 20:12:16 +0900

    24.11.10 20:git config --global user.email sver12@naver.com

---
+++ src/main/java/com/project/muduck/entity/MusicalCastEntity.java
@@ -0,0 +1,27 @@
+package com.project.muduck.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "musical_casts")
+@Table(name = "musical_casts")
+public class MusicalCastEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer castId;
+    private Integer musicalId;
+    private String castName;
+    private String castImage;
+}
